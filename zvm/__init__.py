738c1b44c69f81e05c6200cb729797d5bc6f869c
Liam Bindle <liambindle@gmail.com>
2023-06-29 11:16:45 -0700

    Some old missing work

--- zvm/__init__.py
+++ zvm/__init__.py
@@ -1,3 +1,3 @@
 # from .utils import op, loader, storer, deleter
 # from .vm import run, run_test
-from .zvm import op, loader, storer, deleter, ZVM, State, test
+from .zvm import op, loader, storer, deleter, ZVM, State, test, copier
